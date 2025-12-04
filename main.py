77e7179b6ca6e3906f229c9fbce92a968df1b7f9
Sharick Hernandez <snhernandez79@ucatolica.edu.co>
2025-12-04 08:44:50 -0500

    ahora si fin

--- main.py
+++ main.py
@@ -269,4 +269,7 @@ async def checkout(request: Request, db: AsyncSession = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
