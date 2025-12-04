a2207ae7b24153a32038544be3be015cbdcb89dd
Sharick Hernandez <snhernandez79@ucatolica.edu.co>
2025-12-04 04:55:44 -0500

    este de railway si es

--- main.py
+++ main.py
@@ -252,4 +252,4 @@ async def eliminar_producto(producto_id: int, db: Session = Depends(get_db)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
